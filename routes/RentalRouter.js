d9e6da002a067035750c6077788588b9b6ba73da
mwaller129 <mwaller129@gmail.com>
2023-07-12 01:32:14 -0400

    rental component

--- routes/RentalRouter.js
+++ routes/RentalRouter.js
@@ -2,9 +2,9 @@ const router = require("express").Router()
 const controller = require("../controllers/RentalController")
 const middleware = require("../middleware")
 
-router.get("/", controller.GetRentals)
+router.get("/rentals", controller.GetRentals)
 router.post(
-  "/",
+  "/rentals",
   middleware.stripToken,
   middleware.verifyToken,
   controller.CreateRental
