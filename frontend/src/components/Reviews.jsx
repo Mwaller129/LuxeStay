3c86563ea1f70c83aabd326ff81584829804c063
mwaller129 <mwaller129@gmail.com>
2023-07-12 11:24:33 -0400

    review componenet

---
+++ frontend/src/components/Reviews.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import axios from "axios"
+
+const Reviews = () => {
+  const initialState = {
+    name: "",
+    review: "",
+  }
+
+  const [formState, setFormState] = useState(initialState)
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    await axios.post("http://localhost:3001/rentals/:id", formState)
+    setFormState(initialState)
+  }
+
+  const handleChange = (event) => {
+    setFormState({ ...formState, [event.target.id]: event.target.value })
+  }
+
+  return (
+    // <div><h3>Reviews:</h3></div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          onChange={handleChange}
+          value={formState.name}
+        />
+        <label htmlFor="review">Review: </label>
+        <textarea
+          id="review"
+          cols="30"
+          rows="10"
+          onChange={handleChange}
+          value={formState.review}
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
+}
+export default Reviews
