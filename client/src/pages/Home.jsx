446349e6caaeffe98d98f9a3e102640fc42d7b66
mwaller129 <mwaller129@gmail.com>
2023-07-18 01:22:19 -0400

    fadeIn

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom"
+
 const Home = () => {
   return (
     <body className="main-content">
-      <div className="welcome-scroll">
+      <div className="fadeIn-text">
         <h1>Welcome to LuxeStay</h1>
+        <button className="enterbutton">
+          <Link to="/signin">Enter</Link>
+        </button>
       </div>
     </body>
   )
