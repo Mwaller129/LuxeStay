7ba7f0bc2c1bce51cc9732c1085503fe68004a4d
mwaller129 <mwaller129@gmail.com>
2023-07-17 23:39:36 -0400

    penthouse database

--- client/src/components/RentalDetails.jsx
+++ client/src/components/RentalDetails.jsx
@@ -35,37 +35,36 @@ const rentalDetails = (user) => {
       <div className="rental-grid">
         <div className="rental-header">
           <div className="col">
-            <div className="images">
-              <img
-                style={{ display: "block" }}
-                src={property.image}
-                alt={property.image}
-              ></img>
-            </div>
+            <img
+              style={{ display: "block" }}
+              src={property.image}
+              alt={property.image}
+            ></img>
+          </div>
 
-            <div className="rental-name">
-              <h1>{property.name}</h1>
-            </div>
+          <div className="rental-name">
+            <h1>{property.name}</h1>
           </div>
-          <div className="input-wrapper">
-            <div className="rental-header">
-              <h3>Price: {property.price}</h3>
-              <h3>Bedrooms/Bathrooms: {property.size}</h3>
-            </div>
-            <p>{property.description}</p>
-            <div className="input-wrapper">
-              {/* <Reviews setReviews={setReviews} /> */}
-              <h3>Reviews:</h3>
-              {reviews?.map((review) => (
-                <div key={review._id}>
-                  <p>Name: {review.name} </p>
-                  <p>Review: {review.reviewDetails}</p>
-                </div>
-              ))}
-            </div>
-          </div>
-          <Link to="/rentals/all">Back</Link>
         </div>
+        <div className="input-wrapper">
+          <div className="rental-header">
+            <h3>Price: {property.price}</h3>
+            <h3>Bedrooms/Bathrooms: {property.size}</h3>
+          </div>
+          <p>{property.description}</p>
+          <div className="input-wrapper">
+            {/* <Reviews setReviews={setReviews} /> */}
+            <h3>Reviews:</h3>
+            {reviews?.map((review) => (
+              <div key={review._id}>
+                <p>Name: {review.name} </p>
+                <p>Review: {review.reviewDetails}</p>
+              </div>
+            ))}
+          </div>
+        </div>
+        <Link to="/rentals/all">Back</Link>
+        <Link to="/:rental_id/addreview">Add Review</Link>
       </div>
     </body>
   ) : (
--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -27,24 +27,37 @@ const Reviews = ({ reviews, setReviews }) => {
   }
 
   return (
-    // <div><h3>Reviews:</h3></div>
-    <div className="reviews">
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          onChange={handleChange}
-          value={formState.name}
-        />
-        <label htmlFor="reviewDetails">Review: </label>
-        <textarea
-          id="reviewDetails"
-          onChange={handleChange}
-          value={formState.reviewDetails}
-        ></textarea>
-        <button type="submit">Submit</button>
-      </form>
+    <div className="main-content">
+      <div className="addreview col">
+        <h1>Tell Us About Your Stay!</h1>
+        <p>
+          Complete the form below to let us know about your experience at this
+          property.{" "}
+        </p>
+        <div className="card-overlay centered">
+          <form className="addreview col" onSubmit={handleSubmit}>
+            <div className="input-wrapper">
+              <div className="reviews">
+                <label htmlFor="name">Name:</label>
+                <input
+                  type="text"
+                  id="name"
+                  onChange={handleChange}
+                  value={formState.name}
+                />
+                <label htmlFor="reviewDetails">Review: </label>
+                <input
+                  type="text"
+                  id="reviewDetails"
+                  onChange={handleChange}
+                  value={formState.reviewDetails}
+                ></input>
+                <button type="submit">Submit</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   )
 }
